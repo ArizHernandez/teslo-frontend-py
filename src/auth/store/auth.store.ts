2d4a63b301ba5ecce846dacad477b6bd6e3554b1
Fernando Herrera <fernando.herrera85@gmail.com>
2025-07-26 10:50:52 -0400

    feat: implement authentication check and loading state; add CheckAuthProvider and CustomFullScreenLoading component

--- src/auth/store/auth.store.ts
+++ src/auth/store/auth.store.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import type { User } from '@/interfaces/user.interface';
 
 import { loginAction } from '../actions/login.action';
+import { checkAuthAction } from '../actions/check-auth.action';
 
 type AuthStatus = 'authenticated' | 'not-authenticated' | 'checking';
 
@@ -12,11 +13,12 @@ type AuthState = {
   authStatus: AuthStatus;
 
   // Getters
-  // isAdmin: boolean;
+  isAdmin: () => boolean;
 
   // Actions
   login: (email: string, password: string) => Promise<boolean>;
   logout: () => void;
+  checkAuthStatus: () => Promise<boolean>;
 };
 
 export const useAuthStore = create<AuthState>()((set, get) => ({
@@ -24,6 +26,13 @@ export const useAuthStore = create<AuthState>()((set, get) => ({
   user: null,
   token: null,
   authStatus: 'checking',
+
+  // Getters
+  isAdmin: () => {
+    const roles = get().user?.roles || [];
+    return roles.includes('admin');
+    // return !!get().user?.roles.includes('admin')
+  },
 
   // Actions
   login: async (email: string, password: string) => {
@@ -33,18 +42,40 @@ export const useAuthStore = create<AuthState>()((set, get) => ({
       const data = await loginAction(email, password);
       localStorage.setItem('token', data.token);
 
-      set({ user: data.user, token: data.token });
+      set({ user: data.user, token: data.token, authStatus: 'authenticated' });
 
       return true;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
       localStorage.removeItem('token');
-      set({ user: null, token: null });
+      set({ user: null, token: null, authStatus: 'not-authenticated' });
       return false;
     }
   },
 
   logout: () => {
     localStorage.removeItem('token');
-    set({ user: null, token: null });
+    set({ user: null, token: null, authStatus: 'not-authenticated' });
+  },
+
+  checkAuthStatus: async () => {
+    try {
+      const { user, token } = await checkAuthAction();
+      set({
+        user: user,
+        token: token,
+        authStatus: 'authenticated',
+      });
+      return true;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      set({
+        user: undefined,
+        token: undefined,
+        authStatus: 'not-authenticated',
+      });
+
+      return false;
+    }
   },
 }));
