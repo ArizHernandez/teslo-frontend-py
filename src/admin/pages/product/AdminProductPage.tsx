62e57d41130bc1db6ce2d9851158c8504a0e433e
Fernando Herrera <fernando.herrera85@gmail.com>
2025-07-28 15:04:03 -0400

    feat: implement product creation and update logic; integrate sleep function and mutation handling in AdminProductPage

--- src/admin/pages/product/AdminProductPage.tsx
+++ src/admin/pages/product/AdminProductPage.tsx
@@ -1,44 +1,38 @@
 // https://github.com/Klerith/bolt-product-editor
+import { Navigate, useNavigate, useParams } from 'react-router';
+import { toast } from 'sonner';
 
-import { AdminTitle } from '@/admin/components/AdminTitle';
-import { Navigate, useParams } from 'react-router';
-
-import { useState } from 'react';
-import { X, Plus, Upload, Tag, SaveAll } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { Link } from 'react-router';
 import { useProduct } from '@/admin/hooks/useProduct';
 import { CustomFullScreenLoading } from '@/components/custom/CustomFullScreenLoading';
 import { ProductForm } from './ui/ProductForm';
-
-interface Product {
-  id: string;
-  title: string;
-  price: number;
-  description: string;
-  slug: string;
-  stock: number;
-  sizes: string[];
-  gender: string;
-  tags: string[];
-  images: string[];
-}
+import type { Product } from '@/interfaces/product.interface';
 
 export const AdminProductPage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
-  const {
-    isLoading,
-    isError,
-    data: product,
-    handleSubmitForm,
-  } = useProduct(id || '');
+  const { isLoading, isError, data: product, mutation } = useProduct(id || '');
 
   const title = id === 'new' ? 'Nuevo producto' : 'Editar producto';
   const subtitle =
     id === 'new'
       ? 'Aquí puedes crear un nuevo producto.'
       : 'Aquí puedes editar el producto.';
+
+  const handleSubmit = async (productLike: Partial<Product>) => {
+    await mutation.mutateAsync(productLike, {
+      onSuccess: (data) => {
+        toast.success('Producto actualizado correctamente', {
+          position: 'top-right',
+        });
+        navigate(`/admin/products/${data.id}`);
+      },
+      onError: (error) => {
+        console.log(error);
+        toast.error('Error al actualizar el producto');
+      },
+    });
+  };
 
   if (isError) {
     return <Navigate to="/admin/products" />;
@@ -57,7 +51,8 @@ export const AdminProductPage = () => {
       title={title}
       subTitle={subtitle}
       product={product}
-      onSubmit={handleSubmitForm}
+      onSubmit={handleSubmit}
+      isPending={mutation.isPending}
     />
   );
 };
